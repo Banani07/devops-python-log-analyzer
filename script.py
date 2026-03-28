e322871bffcc963412174508e7d421e18ef4ae60
Banani Manna <manna.banani0704@gmail.com>
2026-03-28 11:02:09 +0530

    Day 2 - Enhanced Log Analyzer with warnings and error percentage

--- script.py
+++ script.py
@@ -1,16 +1,27 @@
-with open("log.txt", "r") as file:
-    error_count = 0
-    info_count = 0
-    total_lines = 0
+try:
+    with open("log.txt", "r") as file:
+        error_count = 0
+        info_count = 0
+        warning_count = 0
+        total_lines = 0
 
-    for line in file:
-        total_lines += 1
+        for line in file:
+            total_lines += 1
 
-        if "ERROR" in line:
-            error_count += 1
-        elif "INFO" in line:
-            info_count += 1
+            if "ERROR" in line:
+                error_count += 1
+            elif "INFO" in line:
+                info_count += 1
+            elif "WARNING" in line:
+                warning_count += 1
 
-print("Total lines:", total_lines)
-print("ERROR:", error_count)
-print("INFO:", info_count)
+    error_percentage = (error_count / total_lines) * 100
+
+    print("Total lines:", total_lines)
+    print("ERROR:", error_count)
+    print("INFO:", info_count)
+    print("WARNING:", warning_count)
+    print("Error %:", round(error_percentage, 2))
+
+except FileNotFoundError:
+    print("Log file not found. Please check the file path.")
